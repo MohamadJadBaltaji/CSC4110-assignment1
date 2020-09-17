57ccb9fd840814606bef3256e75b5d074ec16727
Jadbaltaji <jad_mohammad@hotmail.com>
2020-09-17 18:53:21 -0400

    Changed the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,10 @@
 // Simple C++ program to display "Hello World"
 // Header file for input output functions
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+//Author: Mohamad Jad Baltaji <gr4798@wayne.edu>
+>>>>>>> master
 =======
 //Author: Mohamad Jad Baltaji <gr4798@wayne.edu>
 >>>>>>> master
@@ -14,7 +18,11 @@ int main()
 {
     // prints hello world
 <<<<<<< HEAD
+<<<<<<< HEAD
     cout << "Testing git merge!";
+=======
+    cout << "Hello World!";
+>>>>>>> master
 =======
     cout << "Hello World!";
 >>>>>>> master
