bd87f8259f7fa797aef262a14c6ab5bad0dfb1a6
Jadbaltaji <jad_mohammad@hotmail.com>
2020-09-17 18:26:12 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,15 +1,16 @@
-// Simple C++ program to display "Hello World" 
-// Header file for input output functions 
-#include<iostream>  
-  
-using namespace std; 
-  
-// main function - 
-// where the execution of program begins 
-int main() 
-{ 
-    // prints hello world 
-    cout<<"Hello World!"; 
-      
-    return 0; 
-} 
+// Simple C++ program to display "Hello World"
+// Header file for input output functions
+//Author: Mohamad Jad Baltaji <gr4798@wayne.edu>
+#include <iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World!";
+
+    return 0;
+}
